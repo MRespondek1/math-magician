d07bf460e32523e5d2aa0281aacf4cc05b275c53
MRespondek1 <Resmod23@htl-kaindorf.at>
2026-03-17 11:11:01 +0100

    Welcome to Math Magician

--- main.py
+++ main.py
@@ -1,35 +1,15 @@
 # Math Magician Main File
-# usage: python3 math.py <operator> <operand1> <operand2>
+# usage: python3 main.py <operator> <operand1> <operand2>
+
 import sys
 
-
-def add(a, b):
-    return a + b
-
-
 def main():
-    if len(sys.argv) != 4:
-        print("Usage: python3 math.py <operator> <number1> <number2>")
-        return
-
-    print("Welcome to Math Magician")
-
-    op = sys.argv[1]
+    print("Welcome to Math Magician!")
     
-    try:
-        a = float(sys.argv[2])
-        b = float(sys.argv[3])
-    except ValueError:
-        print("Error: Operands must be numbers.")
-        return
-
-    if op == "+":
-        result = add(a, b)
-    else:
-        print("Invalid operator: use +")
-        return
-
-    print(result)
+    op = sys.argv[1]
+    a = float(sys.argv[2])
+    b = float(sys.argv[3])
+    
 
 if __name__ == "__main__":
     main()
